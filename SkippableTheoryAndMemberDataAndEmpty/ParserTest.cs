58097e391728a1c10ceeaee2dac97a0b21e51dfc
kenjiuno <h8mastre@gmail.com>
2022-10-25 16:12:57 +0900

    as-is

---
+++ SkippableTheoryAndMemberDataAndEmpty/ParserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace SkippableTheoryAndMemberDataAndEmpty
+{
+    public class ParserTest
+    {
+        [SkippableTheory]
+        [MemberData(nameof(ProvideFiles))]
+        public void TestFile(string file)
+        {
+            File.ReadAllText(file); // test this in own code...
+        }
+
+        public static IEnumerable<object[]> ProvideFiles()
+        {
+            foreach (var file in Directory.GetFiles(".", "*.file-extension"))
+            {
+                yield return new object[] { file };
+            }
+        }
+    }
+}
